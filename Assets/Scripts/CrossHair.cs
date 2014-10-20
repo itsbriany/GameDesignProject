ff3494d3a99d480f537660e836d2f7073d48df00
Justin Patriquin <justinpatriquin@gmail.com>
2014-10-20 14:50:02 -0300

    custom cursor
    Added custom cursor texture and script to change cursor.

---
+++ Assets/Scripts/CrossHair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossHair : MonoBehaviour {
+    public Texture2D cursorTexture;
+    CursorMode cursorMode = CursorMode.Auto;
+    Vector2 hotSpot = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        if (!cursorTexture)
+        {
+            Debug.Log("Add cursor texture!");
+            this.enabled = false;
+        }
+         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+}
