5f89844529609985026b47a881c79bc9ad53224d
EvilFuzzball <itsbriany@gmail.com>
2014-10-21 23:53:21 -0300

    Added some metallic textures, raw mocap animation data, human models, and programmed some basic walking animations with root motion by implementing an animator controller.

---
+++ Assets/Scripts/DerikScripts/DerikGravity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DerikGravity : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	    anim = transform.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+		float v = Input.GetAxis("Vertical");				// setup v variables as our vertical input axis
+		anim.SetFloat("Speed", v);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		anim.SetFloat("Direction", h); 	
+	}
+
+    void OnAnimatorMove(){
+        Vector3 velocity = anim.deltaPosition / Time.deltaTime;
+        velocity.y = rigidbody.velocity.y;
+        rigidbody.velocity = velocity;
+    }
+}
