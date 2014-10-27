214092610098df782aaee4edd07252d6f353d951
EvilFuzzball <itsbriany@gmail.com>
2014-10-26 23:25:50 -0300

    The player can now jump and do basic vaults over short obstacles. Physics is now applied to the jumping animation.

--- Assets/Scripts/DerikScripts/DerikGravity.cs
+++ Assets/Scripts/DerikScripts/DerikGravity.cs
@@ -8,12 +8,16 @@ using System.Collections;
 public class DerikGravity : MonoBehaviour {
 
     public float jumpSpeed = 10.0f;
+    public float vaultDistance = 2.0f;
+    public float vaultAccelerateForce = 10000.0f;
 
     private Animator anim;
 
     private AnimatorStateInfo currentBaseState;
     private CapsuleCollider col;
     static int basicJumpState = Animator.StringToHash("Base Layer.DefaultJump");	
+    static int vaultState = Animator.StringToHash("Base Layer.Vault");
+    static int runState = Animator.StringToHash("Base Layer.RunForward");
 	
 	//static int waveState = Animator.StringToHash("Layer2.Wave")
 
@@ -21,6 +25,7 @@ public class DerikGravity : MonoBehaviour {
 	void Start () {
 	    anim = transform.GetComponent<Animator>();
         col = transform.GetComponent<CapsuleCollider>();
+        
        
 	}
 	
@@ -45,19 +50,67 @@ public class DerikGravity : MonoBehaviour {
         //Debug.Log(currentBaseState.ToString());
 
         if(currentBaseState.nameHash == basicJumpState){
-            rigidbody.AddForce(Vector3.up * jumpSpeed * Time.deltaTime, ForceMode.Impulse);
-			if(!anim.IsInTransition(0))
-			{
-				   
-					// ..set the collider height to a float curve in the clip called ColliderHeight
+           // rigidbody.AddForce(Vector3.up * jumpSpeed * Time.deltaTime, ForceMode.Impulse);
+			if(!anim.IsInTransition(0)){
 				col.height = anim.GetFloat("ColliderHeight");
-				
-				// reset the Jump bool so we can jump again, and so that the state does not loop 
-				//anim.SetBool("Jump", false);
 			}
+           // rigidbody.AddForce(rigidbody.transform.forward * vaultAccelerateForce, ForceMode.Impulse);
+        }
+        if(currentBaseState.nameHash == vaultState){
+			if(!anim.IsInTransition(0)){
+				col.height = anim.GetFloat("ColliderHeight");				
+			}
+            anim.SetBool("Vault", false);
+        }
+
+        if(currentBaseState.nameHash == runState){
+            anim.applyRootMotion = true;
         }
         
-		// Raycast down from the center of the character.. 
+        pullDown();
+		
+        vault();
+	}
+
+    void OnCollisionEnter(Collision collision){
+      
+    }
+
+    //Checks for vaulting obstacles
+    void vault(){
+        Ray ray = new Ray(transform.position, rigidbody.transform.forward);
+        Debug.DrawRay(transform.position, rigidbody.transform.forward);
+        
+		RaycastHit hitInfo = new RaycastHit();
+		
+		if (Physics.Raycast(ray, out hitInfo)){
+            if(hitInfo.transform.CompareTag("Obstacle")){
+                if (hitInfo.distance <= vaultDistance){
+				    anim.SetBool("Vault", true);
+				// MatchTarget allows us to take over animation and smoothly transition our character towards a location - the hit point from the ray.
+				// Here we're telling the Root of the character to only be influenced on the Y axis (MatchTargetWeightMask) and only occur between 0.35 and 0.5
+				// of the timeline of our animation clip
+				//anim.MatchTarget(hitInfo.point, Quaternion.identity, AvatarTarget.Root, new MatchTargetWeightMask(new Vector3(0, 1, 0), 0), 0.35f, 0.5f);
+			    }    
+            }
+			// ..if distance to the ground is more than 1.75, use Match Target
+            
+			
+        }
+    }
+
+    //Accelerates the character upon vault
+    void vaultAccelerate(){
+        Debug.Log("Accelerating!");
+        anim.applyRootMotion = false;
+        rigidbody.AddForce(rigidbody.transform.forward * vaultAccelerateForce, ForceMode.Impulse);
+        rigidbody.AddForce(rigidbody.transform.up * vaultAccelerateForce, ForceMode.Impulse);
+    }
+
+    //Pull the character down during animations
+    void pullDown(){
+        // Raycast down from the center of the character.. 
+        
 		Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
 		RaycastHit hitInfo = new RaycastHit();
 		
@@ -73,13 +126,6 @@ public class DerikGravity : MonoBehaviour {
 				anim.MatchTarget(hitInfo.point, Quaternion.identity, AvatarTarget.Root, new MatchTargetWeightMask(new Vector3(0, 1, 0), 0), 0.35f, 0.5f);
 			}
 		}
+    }
 
-	}
-
-    /*
-    void OnAnimatorMove(){
-        Vector3 velocity = anim.deltaPosition / Time.deltaTime;
-        velocity.y = rigidbody.velocity.y;
-       // rigidbody.velocity = velocity;
-    }*/
 }
